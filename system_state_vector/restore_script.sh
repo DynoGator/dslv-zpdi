b04b31c87001654a06dad747535ab1aac85837da
DynoGator Tier-1 Node <jrfross@gmail.com>
2026-08-06 18:23:48 -0600

    chore: remove system config snapshot from tracking (private backup, not source).

--- system_state_vector/restore_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Draft restore script
-sudo dpkg --set-selections < package_selections.txt
-sudo apt-get dselect-upgrade -y
-echo "Manual configurations from 'etc' and 'home_configs' need to be copied carefully!"
